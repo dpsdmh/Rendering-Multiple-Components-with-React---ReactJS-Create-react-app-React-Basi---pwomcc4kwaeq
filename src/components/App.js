8a25653555668b1f646907e34f688a7197ac7f01
Dipanshu Raj <108553225+dpsdmh@users.noreply.github.com>
2022-10-10 18:51:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <h1 data-ns-test="project-name">Portfolio Site</h1>
+      <h3 data-ns-test="project-description">Hi I am Dipanshu Raj and this is the my portfolio website built using react library</h3>
+    </div>
   )
 }
 
